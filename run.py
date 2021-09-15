7bd630eddeb81282fa1fd0a55cbb1661e4fbdce4
lewisbambrick <lewis.bambrick@gmail.com>
2021-09-15 23:35:50 +0000

    create if/else to decide a winner

--- run.py
+++ run.py
@@ -11,6 +11,9 @@ def print_board(board):
         print(" ".join(i))
 
 
+print_board(board)
+
+
 def random_row(board):
     return randint(0, len(board)-1)
 
@@ -19,7 +22,17 @@ def random_col(board):
     return randint(0, len(board)-1)
 
 
+ship_row = random_row(board)
+ship_col = random_col(board)
+
 guess_row = int(input("Guess Row: "))
 
 guess_col = int(input("Guess Colum: "))
 
+
+if guess_row == ship_row and guess_col == ship_col:
+    print("Congratulations! Youve sunk my battleship")
+else:
+    print("Ha!Better luck next time, You missed my battleship.")
+    board[guess_row][guess_col] = "x"
+    print_board(board)
