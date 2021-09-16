f60a202e10ade0d1206aa468637887caa6f007a7
lewisbambrick <lewis.bambrick@gmail.com>
2021-09-16 00:10:18 +0000

    add break at winn condition to ensure code doesn't continue

--- run.py
+++ run.py
@@ -25,19 +25,25 @@ def random_col(board):
 ship_row = random_row(board)
 ship_col = random_col(board)
 
-guess_row = int(input("Guess Row: "))
 
-guess_col = int(input("Guess Colum: "))
+for turn in range(4):
+    print("Turn", turn + 1)
 
+    guess_row = int(input("Guess Row: "))
 
-if guess_row == ship_row and guess_col == ship_col:
-    print("Congratulations! You've sunk my battleship!")
-else:
-    if guess_row > 4 or guess_col > 4:
-        print("Oops that wasn't even in the ocean!")
-    elif board[guess_row][guess_col] == "x":
-        print("You've already guessed there, select another target. ")
+    guess_col = int(input("Guess Colum: "))
+
+    if guess_row == ship_row and guess_col == ship_col:
+        print("Congratulations! You've sunk my battleship!")
+        break
     else:
-        print("Ha!Better luck next time, You missed my battleship.")
-        board[guess_row][guess_col] = "x"
-        print_board(board)
+        if guess_row > 4 or guess_col > 4:
+            print("Oops that wasn't even in the ocean!")
+        elif board[guess_row][guess_col] == "x":
+            print("You've already guessed there, select another target. ")
+        else:
+            print("Ha!Better luck next time, You missed my battleship.")
+            board[guess_row][guess_col] = "x"
+            if turn == 3:
+                print("Game over, you're out of chances!")
+            print_board(board)
